cd0e55ac309aff2ff1c870a8be3f4edd82d25a85
Alpha <alpha.likembe@student.moringaschool.com>
2023-12-05 22:20:22 +0300

    complete

--- server/app.py
+++ server/app.py
@@ -36,6 +36,7 @@ class Plants(Resource):
         db.session.commit()
 
         return make_response(new_plant.to_dict(), 201)
+    
 
 
 api.add_resource(Plants, '/plants')
@@ -46,9 +47,41 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
-
-
+    
 api.add_resource(PlantByID, '/plants/<int:id>')
+  
+
+
+
+class UpdatePlant(Resource):
+    def patch(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+        for key, value in data.items():
+            setattr(plant, key, value)
+
+        db.session.commit()
+
+        return make_response(jsonify(plant.to_dict()), 200)
+
+
+api.add_resource(UpdatePlant, '/plants/<int:id>')
+
+class DeletePlant(Resource):
+    def delete(self, id):
+        plant = Plant.query.get(id)
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+        
+        db.session.delete(plant)
+        db.session.commit()
+
+        return "", 204
+
+api.add_resource(DeletePlant, '/plants/<int:id>')
 
 
 if __name__ == '__main__':
